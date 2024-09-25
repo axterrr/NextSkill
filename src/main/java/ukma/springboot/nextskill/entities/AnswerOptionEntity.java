eb9b2cc406dd55a7297eb818369d3bc860a28cf3
ttriukhan <t.triukhan@ukma.edu.ua>
2024-09-25 21:47:01 +0300

    AnswerOptionEntity updated

--- src/main/java/ukma/springboot/nextskill/entities/AnswerOptionEntity.java
+++ src/main/java/ukma/springboot/nextskill/entities/AnswerOptionEntity.java
@@ -1,4 +1,42 @@
 package ukma.springboot.nextskill.entities;
 
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "answers")
 public class AnswerOptionEntity {
+    @Id
+    @GeneratedValue
+    @Column(nullable = false, updatable = false, unique = true)
+    private final UUID uuid;
+    @Column(nullable = false)
+    private String text;
+    @Column(nullable = false)
+    private boolean isCorrect;
+
+    public AnswerOptionEntity() {
+        this.uuid = UUID.randomUUID();
+    }
+
+    public UUID getId() {
+        return uuid;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public boolean isCorrect() {
+        return isCorrect;
+    }
+
+    public void setCorrect(boolean correct) {
+        isCorrect = correct;
+    }
 }
