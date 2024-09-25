3d07a775474e2b87f9a48d1c4cddca5434d876b9
ttriukhan <t.triukhan@ukma.edu.ua>
2024-09-25 21:25:25 +0300

    CourseEntity updated

--- src/main/java/ukma/springboot/nextskill/entities/CourseEntity.java
+++ src/main/java/ukma/springboot/nextskill/entities/CourseEntity.java
@@ -1,4 +1,61 @@
 package ukma.springboot.nextskill.entities;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "courses")
 public class CourseEntity {
+    @Id
+    @GeneratedValue
+    @Column(nullable = false, updatable = false, unique = true)
+    private final UUID uuid;
+    @Column(nullable = false)
+    private String name;
+    @Column
+    private String description;
+    @Column(nullable = false, updatable = false)
+    private LocalDate creationDate;
+    @ManyToOne
+    @JoinColumn(name = "teacher_id", nullable = false)
+    private UserEntity teacher;
+
+    public CourseEntity() {
+        this.uuid = UUID.randomUUID();
+        creationDate = LocalDate.now();
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getCreationDate() {
+        return creationDate;
+    }
+
+    public UserEntity getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(UserEntity teacher) {
+        this.teacher = teacher;
+    }
 }
--- src/main/java/ukma/springboot/nextskill/entities/PostEntity.java
+++ src/main/java/ukma/springboot/nextskill/entities/PostEntity.java
@@ -1,4 +1,10 @@
 package ukma.springboot.nextskill.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "posts")
 public class PostEntity extends CourseObjectEntity{
+
 }
