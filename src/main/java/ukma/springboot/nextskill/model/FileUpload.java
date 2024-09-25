7d6dcdeaf56e66d5d06ec80185286effee3586cb
rin_r <rafikov.rinat.m@gmail.com>
2024-09-25 23:42:54 +0300

    Added FileUpload dto and entity

---
+++ src/main/java/ukma/springboot/nextskill/model/FileUpload.java
@@ -0,0 +1,65 @@
+package ukma.springboot.nextskill.model;
+
+import ukma.springboot.nextskill.security.FileType;
+
+import java.util.UUID;
+
+public class FileUpload {
+    private UUID id;
+    private FileType type;
+    private String serverUrl;
+    private User owner;
+    private boolean isPublic;
+
+    public FileUpload() {}
+
+    public FileUpload(UUID id, FileType type, String serverUrl, User owner, boolean isPublic) {
+        this.id = id;
+        this.type = type;
+        this.serverUrl = serverUrl;
+        this.owner = owner;
+        this.isPublic = isPublic;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public FileType getType() {
+        return type;
+    }
+
+    public void setType(FileType type) {
+        this.type = type;
+    }
+
+    public String getServerUrl() {
+        return serverUrl;
+    }
+
+    public void setServerUrl(String serverUrl) {
+        this.serverUrl = serverUrl;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public boolean isPublic() {
+        return isPublic;
+    }
+
+    public void setPublic(boolean isPublic) {
+        this.isPublic = isPublic;
+    }
+
+
+}
