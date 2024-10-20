26417e88ca6a3237647cc5c550b6f766bdfb647e
rin_r <rafikov.rinat.m@gmail.com>
2024-10-20 16:11:37 +0300

    Made so spring provides us default login/logout pages

--- src/main/java/ukma/springboot/nextskill/configs/WebSecurityConfig.java
+++ src/main/java/ukma/springboot/nextskill/configs/WebSecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import ukma.springboot.nextskill.security.UserRole;
 
 @Configuration
@@ -24,15 +25,15 @@ public class WebSecurityConfig {
                         .requestMatchers("/api/**").authenticated()
                         .anyRequest().permitAll()
                 )
-                .formLogin((form) -> form
-                        .loginProcessingUrl("/login")
-                        .permitAll()
-                )
+                .formLogin((form) -> form.permitAll())
                 .logout((logout) -> logout.permitAll())
                 .exceptionHandling((exceptions) -> exceptions
-                        .authenticationEntryPoint((request, response, authException) -> {
-                            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
-                        })
+                        .defaultAuthenticationEntryPointFor(
+                                (request, response, authException) -> {
+                                    response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+                                },
+                                new AntPathRequestMatcher("/api/**")
+                        )
                 );
 
         return http.build();
