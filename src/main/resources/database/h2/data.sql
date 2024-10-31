f5b1087c7698783029a2a64615cd895bba697d68
rin_r <rafikov.rinat.m@gmail.com>
2024-10-31 02:40:14 +0200

    Fixed 403 Forbidden for h2-console and auth login.

--- src/main/resources/database/h2/data.sql
+++ src/main/resources/database/h2/data.sql
@@ -5,7 +5,7 @@ INSERT INTO roles (id, title) VALUES
 
 INSERT INTO users (uuid, username, name, surname, email, phone, avatar_link, description, created_at, is_disabled, password_hash)
 VALUES
-    (UUID(), 'test_user', 'User', 'Test', 'test_user@test.com', '1234567890', NULL, 'Test data.', NOW(), FALSE, '$2y$10$FMzzXACq4tAE2Bn/o.EN8eL4By9CzB3ZMH7iAGMyOvvHmFszKqym2');
+    (UUID(), 'test_user', 'User', 'Test', 'test_user@test.com', '1234567890', NULL, 'Test data.', NOW(), FALSE, '$2y$10$lack10NYc8XCjDBQygtmZODE3da6yELCQof5cdifRsILycTG4ShNi');
 
 INSERT INTO user_roles (user_id, role_id)
 VALUES
