69b16fd2348b0d7c73da5a6f2cc8dc82f0a339c9
rin_r <rafikov.rinat.m@gmail.com>
2024-11-27 02:41:08 +0200

    Added hot swapping

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,6 +34,9 @@ dependencies {
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    // Hot reload
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
 }
 
 tasks.withType<Test> {
