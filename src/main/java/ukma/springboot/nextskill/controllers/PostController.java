917230c1ae66f98ecd3f7d852681665a89625552
haryok123 <kharyshyn.igor@gmail.com>
2024-11-28 14:49:40 +0200

    add-post

--- src/main/java/ukma/springboot/nextskill/controllers/PostController.java
+++ src/main/java/ukma/springboot/nextskill/controllers/PostController.java
@@ -2,10 +2,12 @@ package ukma.springboot.nextskill.controllers;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import ukma.springboot.nextskill.models.enums.UserRole;
 import ukma.springboot.nextskill.models.responses.UserResponse;
+import ukma.springboot.nextskill.models.views.PostView;
+import ukma.springboot.nextskill.models.views.UserView;
 import ukma.springboot.nextskill.services.CourseService;
 import ukma.springboot.nextskill.services.PostService;
 import ukma.springboot.nextskill.services.UserService;
@@ -27,11 +29,31 @@ public class PostController {
 
         UserResponse authenticated = userService.getAuthenticatedUser();
         boolean isOwner = courseService.hasOwnerRights(authenticated.getUuid(), courseId);
-        if(!isOwner && authenticated.getRole() != UserRole.ADMIN)
+        if (!isOwner && authenticated.getRole() != UserRole.ADMIN)
             return "redirect:/course/" + courseId;
 
         postService.delete(postId);
 
         return "redirect:/course/" + courseId + "?post&deleted";
     }
+
+    @GetMapping("/section/{sectionId}/{courseId}/addPost")
+    public String showPostForm(@PathVariable("sectionId") UUID sectionId, @PathVariable("courseId") UUID courseId, Model model) {
+        model.addAttribute("post", PostView.builder().sectionId(sectionId).build());
+        model.addAttribute("sectionId", sectionId);
+        model.addAttribute("courseId", courseId);
+        return "add-post";
+    }
+
+    @PostMapping("/section/{sectionId}/addPost")
+    public String addPostToSection(@PathVariable UUID sectionId,
+                                   @RequestParam String name,
+                                   @RequestParam String content,
+                                   @RequestParam(required = false) Boolean isHidden,
+                                   @RequestParam UUID courseId) {
+        return "redirect:/course/" + courseId;
+    }
+
+
+
 }
