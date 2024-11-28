2370cb3f735cc1329bca9def382c4e76ca46ae91
Tetiana Triukhan <t.triukhan@ukma.edu.ua>
2024-11-28 15:16:20 +0200

    tests started

--- src/test/java/controllers/CourseControllerTest.java
+++ src/test/java/controllers/CourseControllerTest.java
@@ -20,7 +20,7 @@ import java.util.UUID;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
-class CoursesControllerTest {
+class CourseControllerTest {
 
     @Mock
     private UserService userService;
