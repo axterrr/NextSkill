b67d57172f8d5b00acc03f699a390e6fe54e2f01
haryok123 <kharyshyn.igor@gmail.com>
2024-11-28 11:20:44 +0200

    fixed endpoints

--- src/main/java/ukma/springboot/nextskill/controllers/AuthController.java
+++ src/main/java/ukma/springboot/nextskill/controllers/AuthController.java
@@ -1,20 +1,42 @@
 package ukma.springboot.nextskill.controllers;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import ukma.springboot.nextskill.models.responses.UserResponse;
+import ukma.springboot.nextskill.services.UserService;
+import ukma.springboot.nextskill.models.views.UserView;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class AuthController {
 
+    private UserService userService;
+
     @GetMapping("login")
     public String login() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication != null  && !(authentication instanceof AnonymousAuthenticationToken)) {
+        if (authentication != null && !(authentication instanceof AnonymousAuthenticationToken)) {
             return "redirect:/home";
         }
         return "login";
     }
+
+
+//        @PostMapping("/register")
+//        public ResponseEntity<UserResponse> registerUser (@RequestBody UserView userView){
+//            UserResponse newUser = userService.create(userView);
+//            return ResponseEntity.status(201).body(newUser);
+//        }
+
+        @GetMapping("register")
+        public String register() {
+
+            return "register";
+        }
+
 }
