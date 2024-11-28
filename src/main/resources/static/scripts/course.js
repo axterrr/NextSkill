7ffd2f98c5da06d7fa7bb2e429b4efe06b20acc8
Tetiana Triukhan <t.triukhan@ukma.edu.ua>
2024-11-28 06:42:57 +0200

    Course page updated

--- src/main/resources/static/scripts/course.js
+++ src/main/resources/static/scripts/course.js
@@ -6,3 +6,21 @@ function toggleContent(button) {
         sectionContent.style.display = "none";
     }
 }
+
+function toggleSettings() {
+    const sectionContent = document.getElementById('settings-buttons');
+    if (sectionContent.style.display === "none" || sectionContent.style.display === "") {
+        sectionContent.style.display = "flex";
+    } else {
+        sectionContent.style.display = "none";
+    }
+}
+
+function toggleSectionSettings(button) {
+    const sectionContent = button.previousElementSibling;
+    if (sectionContent.style.display === "none" || sectionContent.style.display === "") {
+        sectionContent.style.display = "flex";
+    } else {
+        sectionContent.style.display = "none";
+    }
+}
