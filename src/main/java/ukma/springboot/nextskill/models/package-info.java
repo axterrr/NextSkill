8fbfca9b5543d6a7c7c2c6b0a9894c48b5d6952c
r-rin <rafikov.rinat.m@gmail.com>
2025-09-10 22:49:19 +0300

    made models module public as a temporary solution

---
+++ src/main/java/ukma/springboot/nextskill/models/package-info.java
@@ -0,0 +1,6 @@
+@org.springframework.modulith.ApplicationModule(
+        type = ApplicationModule.Type.OPEN
+)
+package ukma.springboot.nextskill.models;
+
+import org.springframework.modulith.ApplicationModule;
