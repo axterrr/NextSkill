4d3560d28bdd3bc6a90a1be7508aaf5706d373d0
Vladyslav Hibskyi <vgibskiy@gmail.com>
2025-09-19 20:04:43 +0300

    fix

--- src/main/java/ukma/springboot/nextskill/email/EmailManagement.java
+++ src/main/java/ukma/springboot/nextskill/email/EmailManagement.java
@@ -1,9 +1,9 @@
 package ukma.springboot.nextskill.email;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.event.TransactionalEventListener;
 import ukma.springboot.nextskill.email.service.EmailService;
 
 @Service
@@ -13,7 +13,7 @@ public class EmailManagement {
     private final EmailService emailService;
 
     @Async
-    @TransactionalEventListener
+    @EventListener
     void on(EmailSendEvent event) {
         emailService.sendEmail(event.getRecipient(), event.getSubject(), event.getText());
     }
